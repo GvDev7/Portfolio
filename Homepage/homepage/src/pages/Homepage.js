572408ddd7b5c3df448941ff74471761f305eba7
Aaron <a.genevision14@gmail.com>
2021-11-19 17:30:53 -0600

    inserted data into software engineer section and styled section

--- Homepage/homepage/src/pages/Homepage.js
+++ Homepage/homepage/src/pages/Homepage.js
@@ -18,12 +18,40 @@ const Homepage = (props) => {
             <div className="frames">
                 <div className="firstFrame">
                     <h4>Software Engineer</h4>
-                    <p></p>
+                    <ul>
+                        <li>Certified in Web Design from DeVry University @2018
+                            <ul>
+                                <li>-K.I.S.S</li>
+                                <li>-Emphais on what matters</li>
+                                <li>-Fitt's Law</li>
+                                <li>-Hick's Law</li>
+                                <li>-Miller's Law</li>
+                            </ul>
+                        </li>
+                        <li>Self taught Web Developer @2019
+                            <ul>
+                                <li>HTML and CSS</li>
+                                <li>JavaScript</li>
+                                <li>JQuery</li>
+                                <li>ReactJs and Angular</li>
+                            </ul>
+                        </li>
+                        <li>Applications Developer @2020
+                            <ul>
+                                <li>PHP</li>
+                                <li>Python</li>
+                                <li>Java</li>
+                                <li>Spring boot</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
+                <br/>
                 <div className="secondFrame">
                     <h4>Designer</h4>
                     <p></p>
                 </div>
+                <br/>
                 <div className="thirdFrame">
                     <h4>Consultant</h4>
                     <p></p>
